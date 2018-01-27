65bda3a703975e069dbe17e95b5434b5ce629257
Anddy Archer <adarcher@gmail.com>
2018-01-27 12:56:24 -0800

    Change look to turn_turret

--- tools/json_design.js
+++ tools/json_design.js
@@ -1,28 +1,43 @@
 $(function() {
-    var cards_json = "../client/Assets/Resources/json/cards.json";
-    var actions_json = "../client/Assets/Resources/json/actions.json";
-    var tiles_json = "../client/Assets/Resources/json/tiles.json";
-    var cards;
-    var actions;
-    var tiles;
-    $.getJSON(cards_json, function(json) {
-       cards = json;
-    });
-    $.getJSON(actions_json, function(json) {
-       actions = json;
-    });
-    $.getJSON(tiles_json, function(json) {
-       tiles = json;
-    });
+    var data_types = ["card", "action", "tile", "tile_add_on", "map"];
 
-    var cards_button = "<button class='tool'>Cards</button>";
-    cards_button = $(cards_button).click(function() {
-        $(".tool_workspace").empty().append("HI");
-    });
-    var actions_button = "<button class='tool'>Actions</button>";
-    var tiles_button = "<button class='tool'>Tiles</button>";
+    var tools = {};
 
-    $(".tool_list").append(cards_button);
-    $(".tool_list").append(actions_button);
-    $(".tool_list").append(tiles_button);
+    function SetupWorkspace(tool) {
+        var $workspace = $(".tool_workspace").empty();
+
+        $workspace.append("<div class='header'>" + tool.name + "</div>");
+        Object.keys(tool.schema).forEach((key) => {
+            var value = tool.schema[key];
+            $workspace.append('<span class="key">' + key + '</span>');
+            $workspace.append('<input type="text" placeholder="' + value + '" value="' + value + '"/>');
+            $workspace.append('<br/>');
+        });
+    };
+
+    $(data_types).each(function(i, tool_name) {
+        var tool = {};
+        tool.name = tool_name;
+        var prototype_json = "../client/Assets/Resources/json/prototypes/" + tool_name + ".json";
+        $.getJSON(prototype_json, function(json) {
+            tool.prototype = json;
+            console.log( "Loaded " + tool_name + " prototype." );
+        });
+        var schema_json = "../client/Assets/Resources/json/schema/" + tool_name + ".json";
+        $.getJSON(schema_json, function(json) {
+            tool.schema = json;
+            console.log( "Loaded " + tool_name + " schema." );
+        });
+        var tool_json = "../client/Assets/Resources/json/" + tool_name + "s.json";
+        $.getJSON(tool_json, function(json) {
+            tool.instances = json;
+            console.log( "Loaded " + tool_name + " instances." );
+        });
+        tool.button = $("<button class='tool'>" + tool_name + "</button>").click(() => {
+           SetupWorkspace(tool);
+        });
+
+        tools[tool_name] = tool;
+        $(".tool_list").append(tool.button);
+    });
 });
