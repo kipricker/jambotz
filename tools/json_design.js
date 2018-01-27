f61489e168c5d441003d8ed0c835e9f82a102032
Anddy Archer <adarcher@gmail.com>
2018-01-27 01:28:42 -0800

    Json tools work kinda

--- tools/json_design.js
+++ tools/json_design.js
@@ -1,7 +1,7 @@
 $(function() {
-    var cards_json = "../json/cards.json";
-    var actions_json = "../json/actions.json";
-    var tiles_json = "../json/tiles.json";
+    var cards_json = "../client/Assets/Resources/json/cards.json";
+    var actions_json = "../client/Assets/Resources/json/actions.json";
+    var tiles_json = "../client/Assets/Resources/json/tiles.json";
     var cards;
     var actions;
     var tiles;
@@ -14,4 +14,15 @@ $(function() {
     $.getJSON(tiles_json, function(json) {
        tiles = json;
     });
+
+    var cards_button = "<button class='tool'>Cards</button>";
+    cards_button = $(cards_button).click(function() {
+        $(".tool_workspace").empty().append("HI");
+    });
+    var actions_button = "<button class='tool'>Actions</button>";
+    var tiles_button = "<button class='tool'>Tiles</button>";
+
+    $(".tool_list").append(cards_button);
+    $(".tool_list").append(actions_button);
+    $(".tool_list").append(tiles_button);
 });
