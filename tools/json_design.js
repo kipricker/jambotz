a2795c700d97c563c6b9f38b405375fd5279499e
Anddy Archer <adarcher@gmail.com>
2018-01-27 13:12:58 -0800

    Fix json syntax

--- tools/json_design.js
+++ tools/json_design.js
@@ -3,12 +3,14 @@ $(function() {
 
     var tools = {};
 
-    function SetupWorkspace(tool) {
+    function SetupWorkspace(tool, item) {
         var $workspace = $(".tool_workspace").empty();
 
         $workspace.append("<div class='header'>" + tool.name + "</div>");
         Object.keys(tool.schema).forEach((key) => {
-            var value = tool.schema[key];
+            var value = item[key];
+            var enabled = item[key] != undefined;
+            $workspace.append('<input type="checkbox" state="' + enabled + '"/>');
             $workspace.append('<span class="key">' + key + '</span>');
             $workspace.append('<input type="text" placeholder="' + value + '" value="' + value + '"/>');
             $workspace.append('<br/>');
@@ -34,7 +36,11 @@ $(function() {
             console.log( "Loaded " + tool_name + " instances." );
         });
         tool.button = $("<button class='tool'>" + tool_name + "</button>").click(() => {
-           SetupWorkspace(tool);
+            var more_than_one = false;
+            Object.keys(tool.instances).forEach((key) => {
+                if (more_than_one)
+                SetupWorkspace(tool, tool.instances[key]);
+            })
         });
 
         tools[tool_name] = tool;
