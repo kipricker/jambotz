1a1174a495885958093d857468a0ccd67dae5115
Konstantin <konstantinkh@hotmail.com>
2018-01-26 22:10:51 -0800

    Add status to Bot.

--- client/Assets/Scripts/Bot.cs
+++ client/Assets/Scripts/Bot.cs
@@ -4,11 +4,20 @@ using UnityEngine;
 
 public class Bot : MonoBehaviour {
 
+    public enum Status
+    {
+        Idle = 0,
+        Moving,
+        Turning,
+        Action
+    };
+
     public GameObject m_arena;
     public int m_x_position;
     public int m_y_position;
 
     private int m_orientation = 0;
+    private Status m_status = Status.Idle;
 
 	// Use this for initialization
 	void Start () {
@@ -17,9 +26,15 @@ public class Bot : MonoBehaviour {
         gameObject.transform.parent = m_arena.transform;
         gameObject.transform.position = new Vector3(arena.GridX(m_x_position), 50.0f, arena.GridY(m_y_position));
 	}
+
+    public Status GetStatus()
+    {
+        return m_status;
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
 }
