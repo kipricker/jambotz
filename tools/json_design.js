ef67aa354a8c5eda81f25eec248b8197362d5430
Anddy Archer <adarcher@gmail.com>
2018-01-27 20:13:12 -0800

    shot == fire

--- tools/json_design.js
+++ tools/json_design.js
@@ -35,6 +35,74 @@ $(function() {
         });
     };
 
+    function SetupMapSpace(map) {
+        var $workspace = $(".tool_space");
+        $workspace.empty();
+        $(".tool_header").empty().append(map.name);
+    
+        // //
+        // "x" : 0, "y" : 0,
+        // "tile" : "tile_name",
+        // "tile_add_ons" : [
+        //     {
+        //         "name" : "tile_add_on_name",
+        //         "trigger" : {
+        //             "name" : "trigger_link_name",
+        //             "actions" : []
+        //         },
+        //         "edge" : "north",
+        //         "start_state" : "default"
+        //     }
+        // ],
+        // "orientation" : "vertical",
+        // "variation" : "tile_variation"
+
+        var map_layout = [];
+        Object.values(map.map_data).forEach((tile) => {
+            var cell = "<div class='tile'></div>";
+            var laser = "<div class='laser'></div>";
+            $(cell).append(laser);
+            $(cell).prop("top", tile.x + "px");
+            $(cell).prop("left", tile.y + "px");
+
+            $(cell).addClass(tile.tile);
+
+            Object.values(tile.tile_add_ons).forEach((add_on) => {
+                switch(add_on.name) {
+                    case "wall":
+                        $(cell).addClass('tile_wall_' + add_on.edge);
+                        break;
+                    case "laser":
+                        $(laser).addClass('tile_laser_' + add_on.edge);
+                        break;
+                }
+            })
+            $workspace.append(cell);
+        });
+        
+        Object.keys(tool.schema).forEach((key) => {
+            var value = item[key];
+            var enabled = value != undefined;
+            if (value == undefined)
+                value = tool.schema[key];
+
+            var $item = $('<div class="variable"></div>');
+            $item.append('<span class="key">' + key + '</span>');
+            if (typeof tool.schema[key] == "boolean") {
+                $item.append('<input class="text" type="checkbox" ' + (value ? "checked" : "unchecked") + " " + (enabled ? "enabled" : "disabled") + '/>');
+
+            } else {
+                $item.append('<input class="text" type="text" placeholder="' + value + '" value="' + value + '" ' + (enabled ? "enabled" : "disabled") + '/>');
+            }
+            $item.append($('<input class="check" type="checkbox" ' + (enabled ? "checked" : "unchecked") + '/>').change(function() {
+                var enabled = $(this).prop( "checked" );
+                $(this).siblings(".text").prop('disabled', !enabled);
+            }));
+            $item.append('<br/>');
+            $workspace.append($item);
+        });
+    };
+
     $(data_types).each(function(i, tool_name) {
         var tool = {};
         tool.name = tool_name;
@@ -67,15 +135,27 @@ $(function() {
         $(".tools").append(tool.button);
     });
 
-    var maps;
+    var mapper = $("<select name='maps'><option></option></select>");
+
+    var maps = {};
+    var maps_json = "../client/Assets/Resources/json/maps.json";
     $.getJSON(maps_json, function(json) {
-        maps = json;
-        console.log( "Loaded maps." );
+        var map_names = json.maps;
+        console.log( "Loaded map names." );
+
+        Object.values(map_names).forEach((map_name) => {
+            var map_json = "../client/Assets/Resources/json/maps/" + map_name + ".json";
+            $.getJSON(map_json, function(json) {
+                if (json.name == undefined)
+                    json.name = map_name;
+                maps[map_name] = json;
+                $(mapper).append("<option value='" + map_name + "'>" + json.name + "</option>");
+            });
+        });
     });
 
-    var mapper = $("<select name='maps'></select>");
-    Object.keys(maps).forEach((key, map_name) => {
-        $(mapper).append("<option value='" + map_name + "'>");
-    })
+    $(mapper).change(function() {
+        SetupMapSpace(maps[this.value]);
+    });
     $(".tools").append(mapper);
 });
